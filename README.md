864eada4fcb9af50f2f7c904c390ab7f6b21b1b6
Chandrashekhar0106 <saraswatchandra2@gmail.com>
2023-06-06 15:10:31 +0530

    this commit includes updated application

--- README.md
+++ README.md
@@ -1,17 +1,1 @@
-# ml_credit_card_default
-
-### Tools requirement
-
-1. [githubAccount]
-2. [HerokuAccount] 
-3. [VSCodeIDE]
-4. [GitCLI]
-
-
-Create a new enviorment 
-
-'''
-
-conda create -p venv python==3.8 -y
-
-'''
+## Credit Card Default
--- app.py
+++ app.py
@@ -1,40 +1,63 @@
-import json
-import pickle
-from flask import Flask,request,app,jsonify,url_for,render_template
+from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 
-app=Flask(__name__)
-## Load the model
-xgboostc=pickle.load(open('xgb_c.pkl','rb'))
-scalar=pickle.load(open('scaling.pkl','rb'))
-@app.route('/')
-def home():
-    return render_template('home.html')
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    data=request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=xgboostc.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+application=Flask(__name__)
 
+#app=application
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    data=[float(x) for x in request.form.values()]
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    output=xgboostc.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The person will  {}".format(output))
+## Route for a home page
 
+@application.route('/')
+def index():
+    return render_template('index.html') 
 
+@application.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            LIMIT_BAL=float(request.form.get('LIMIT_BAL')),
+            SEX=int(request.form.get('SEX')),
+            EDUCATION=int(request.form.get('EDUCATION')),
+            MARRIAGE=int(request.form.get('MARRIAGE')),
+            AGE=int(request.form.get('AGE')),
+            PAY_0 = int(request.form.get('PAY_0')),
+            PAY_2=int(request.form.get('PAY_2')),
+            PAY_3=int(request.form.get('PAY_3')),
+            PAY_4=int(request.form.get('PAY_4')),
+            PAY_5=int(request.form.get('PAY_5')),
+            PAY_6=int(request.form.get('PAY_6')),
+            BILL_AMT1=float(request.form.get('BILL_AMT1')),
+            BILL_AMT2=float(request.form.get('BILL_AMT2')),
+            BILL_AMT3=float(request.form.get('BILL_AMT3')),
+            BILL_AMT4=float(request.form.get('BILL_AMT4')),
+            BILL_AMT5=float(request.form.get('BILL_AMT5')),
+            BILL_AMT6=float(request.form.get('BILL_AMT6')),
+            PAY_AMT1=float(request.form.get('PAY_AMT1')),
+            PAY_AMT2=float(request.form.get('PAY_AMT2')),
+            PAY_AMT3=float(request.form.get('PAY_AMT3')),
+            PAY_AMT4=float(request.form.get('PAY_AMT4')),
+            PAY_AMT5=float(request.form.get('PAY_AMT5')),
+            PAY_AMT6=float(request.form.get('PAY_AMT6'))
+            
+            
 
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
 
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
 
 if __name__=="__main__":
-    app.run(debug=True)
-   
+    application.run(host="0.0.0.0", port=5000, debug=True)       
